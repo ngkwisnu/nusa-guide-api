40c5527bbecb51f88e3f59f0e21071a1af9e16b9
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-06-24 12:05:54 +0800

    Switch Database

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const dbPool = mysql.createPool({
   host: "sql12.freesqldatabase.com",
-  user: "sql12714367",
-  password: "8csKtDY2yZ",
-  database: "sql12714367",
+  user: "sql12715703",
+  password: "BGRUHNLSKG",
+  database: "sql12715703",
 });
 
 module.exports = dbPool.promise();
